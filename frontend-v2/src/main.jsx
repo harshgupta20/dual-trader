ba628cbb3c4260f2d3577d95701f94e1d0cd4447
harshgupta20 <hgreatgupta2001@gmail.com>
2025-07-19 20:40:19 +0530

    feat: remove ThemeProvider wrapper from main component

--- frontend-v2/src/main.jsx
+++ frontend-v2/src/main.jsx
@@ -3,15 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from '@mui/material'
-import theme from './theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
